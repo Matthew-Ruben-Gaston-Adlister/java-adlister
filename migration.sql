3f2c4d161324684e91264ce84fc93e5bb9c56802
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-03-09 16:17:23 -0500

    Added to the migration.sql to include a categories table and ads_categories table.

--- migration.sql
+++ migration.sql
@@ -2,21 +2,37 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ads_categories;
 
 CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ad_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    PRIMARY KEY (ad_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(64),
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE ads_categories (
+    ad_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads (ad_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (ad_id, category_id)
 );
